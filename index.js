d50523c2d560fa59ac8ee386760d834acee1c1c0
Reza H <rezahnamavar@gmail.com>
2018-03-26 15:43:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 var recipes = new Object()
+=======
+>>>>>>> 0bff2b00135b01a812f83064a791085e713ffff9
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object, {[key]: value})
